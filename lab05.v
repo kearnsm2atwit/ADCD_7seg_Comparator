936ae293e8ed719ffccad7a5599e1807bab5cbcc
kearnsm2atwit <55287446+kearnsm2atwit@users.noreply.github.com>
2020-06-18 17:05:33 -0400

    Update lab05.v

--- lab05.v
+++ lab05.v
@@ -1,6 +1,6 @@
 module lab05(input [3:0]A, B, input C, output [7:0]Z1, Z2, Z3);
 
-	reg [3:0]X;									// Register to use as input for HEX[0]
+	reg [3:0]X;						// Register to use as input for HEX[0]
 	sevenseg segone(A, Z1);					// Display input 1 on HEX[4]
 	sevenseg segthree(B, Z3);				// Display input 2 on HEX[2]
 	sevenseg segtwo(X, Z2);					// Display min/max on HEX[0]
@@ -10,11 +10,11 @@ module lab05(input [3:0]A, B, input C, output [7:0]Z1, Z2, Z3);
 		
 		begin						
 			if(C == 1'b0 && A > B)			// If the button is pressed and A is the max
-				X = A;							// Display A on HEX[0]
-			else if (C == 1'b1 && A < B)	// If the button is not pressed and A is the min
-				X = A;							// Display A on HEX[0]
-			else									// Else statement covers both cases for displaying B
-				X = B;							// Display B on HEX[0]
-		end										// End always block
+				X = A;				// Display A on HEX[0]
+			else if (C == 1'b1 && A < B)		// If the button is not pressed and A is the min
+				X = A;				// Display A on HEX[0]
+			else					// Else statement covers both cases for displaying B
+				X = B;				// Display B on HEX[0]
+		end						// End always block
 		
 endmodule
